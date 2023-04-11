46ee917cf845d56083d721853c35b8124a47d33a
winwin2671 <88149505+winwin2671@users.noreply.github.com>
2023-04-11 03:10:26 +0000

    completed a_square

---
+++ half_of_a_square/a_square.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using std::cin;
+using std::cout;
+
+int main()
+{
+
+    for (int row = 1; row <= 5; row++)
+    {
+
+        for (int hashNum = 1; hashNum <= 5; hashNum++)
+        {
+            cout << "#";
+        }
+        cout << "\n";
+    }
+}
